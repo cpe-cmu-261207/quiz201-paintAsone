b486ac33202a7bb556a8063cc95aee19cd120012
Paint Natkamoma <48906427+paintAsone@users.noreply.github.com>
2021-07-29 14:10:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,34 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
-// define more constants and variables here
+const text = document.querySelector('#text')
+const searchbtn = document.querySelector('#search')
+const resetbtn = document.querySelector('#reset')
+const originaltext = text.innerText
 
-btn_toggle.onclick = () => {
-  // your code here
+function toggle(){
+  author.innerHTML = 630610728 + length.value
 }
+btn_toggle.addEventListener('click', function(){
+  if(author.innerHTML === "630610728 NATKAMONLAK INTARAMANON"){
+    var x = parseInt("630610728")
+    author.innerHTML = x+5
+  }
+  else{
+    author.innerHTML = "630610728 NATKAMONLAK INTARAMANON"
+  }
+})
 
-// more codes for Search and Reset buttons here
+
+function searching(){
+
+}
+searchbtn.addEventListener('click', searching)
+
+
+function resetinput(){
+  length.value = 5
+  color.value = "#FF0000"
+  text.innerText = originaltext
+}
+resetbtn.addEventListener('click', resetinput)
